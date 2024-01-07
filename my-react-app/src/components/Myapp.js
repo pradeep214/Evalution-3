79da2461e81a723ee0876fffbfa68472f05eb14d
Pradeep Dhakad <dhakadpradeep39@gmail.com>
2024-01-07 14:48:10 +0530

    app

---
+++ my-react-app/src/components/Myapp.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Myapp() {
+  return <div className="app-style">
+    <h1>My React App</h1>
+<ul>
+  <li>Home</li>
+  <li>About</li>
+  <li>Contact</li>
+  <li>Info</li>
+</ul>
+  </div>;
+}
+
+export default Myapp;
